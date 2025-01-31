690775d77cde417774eccf091cb2e98964b8c2cd
Akikoaok <akikoaok@example.com>
2025-01-31 18:37:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import json
+
+app = FastAPI()
+
+# Создаем объект для рендеринга шаблонов
+templates = Jinja2Templates(directory="templates")
+
+# Храним сообщения в памяти
+messages = []
+counter = 1
+
+# Отправка сообщений через WebSocket
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            message_data = json.loads(data)
+            global counter
+            message = {"id": counter, "content": message_data['content']}
+            counter += 1
+            messages.append(message)
+            await websocket.send_text(json.dumps(messages))  # Отправка всех сообщений обратно
+    except WebSocketDisconnect:
+        pass
+
+# Отправка HTML страницы с клиентским кодом
+@app.get("/")
+async def get(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Запуск приложения
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
